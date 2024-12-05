34a268fe9d5b33fb14f3ff5035d28e5f29d8f320
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-05 13:45:34 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/guguamanda" target="_blank">
-          Gugulethu Amanda Ndlovu{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/guguamanda/react-weather-app-shecodes"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/guguamanda"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Gugulethu Amanda Ndlovu{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/guguamanda/react-weather-app-shecodes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
