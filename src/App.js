de675f4f85600f49ee523f78efd5e1c4f22191ae
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-05 13:08:41 +0200

    Added Github

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/guguamanda" target="_blank">
+          Gugulethu Amanda Ndlovu{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/guguamanda/react-weather-app-shecodes"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
