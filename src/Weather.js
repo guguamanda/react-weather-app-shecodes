c8f0571d216c37a96b23c83d82df87235ed37f20
g-ndlovu <langathiwendlovu97@gmail.com>
2024-12-07 12:19:07 +0200

    Added react-animated-weather-icons and added weather temperature and the unit conversion

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
@@ -17,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.temperature.humidity,
       date: new Date(response.data.time * 1000),
       description: response.data.condition.description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain_light.png",
+      icon: response.data.condition.icon,
       wind: response.data.wind.speed,
       city: response.data.city,
     });
